0a1e73f281cfe4142f5e63b0bbcabdefc694394a
Wi11iamC <willi4m553@gmail.com>
2022-07-22 22:36:27 -0400

    :rocket: initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import "../styles/global.css"
+
+// styles
+const pageStyles = {
+  color: "#fff",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
+}
+const headingAccentStyles = {
+  color: "#fff",
+}
+const paragraphStyles = {
+  marginBottom: 48,
+}
+const codeStyles = {
+  color: "#8A6534",
+  padding: 4,
+  backgroundColor: "#FFF4DB",
+  fontSize: "1.25rem",
+  borderRadius: 4,
+}
+
+const IndexPage = () => {
+  return (
+    <main style={pageStyles}>
+      <title>Home Page</title>
+      <h1 style={headingStyles}>
+        Congratulations
+        <br />
+        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
+        🎉🎉🎉
+      </h1>
+      <p style={paragraphStyles}>
+        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
+        update in real-time. 😎
+      </p>
+    </main>
+  )
+}
+
+export default IndexPage
